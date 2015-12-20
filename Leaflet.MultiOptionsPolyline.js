c1c7a9dd6317243e47070c1af9e56c9c1c91de21
Marek Janda <mjanda@users.noreply.github.com>
2015-12-20 16:29:31 +0100

    Update Leaflet.MultiOptionsPolyline.js

--- Leaflet.MultiOptionsPolyline.js
+++ Leaflet.MultiOptionsPolyline.js
@@ -1,121 +1,116 @@
 /*global L */
 
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-/*
- * L.MultiOptionsPolyline is a MultiPolyLine which parts can be styled differently.
- * options: {
- *     multiOptions: {
- *         optionIdxFn: function (latLng, prevLatLng, index, allLatlngs),
- *         fnContext: ctx, // the context to call optionIdxFn (optional)
- *         options: [{}, {}, {}] or function, // options for the index returned by optionIdxFn. If supplied with a function then it will be called with the index
- *         copyBaseOptions: true
- *     },
- *     // other options from Polyline
- * }
- */
-L.MultiOptionsPolyline = L.FeatureGroup.extend({
+  /*
+   * L.MultiOptionsPolyline is a MultiPolyLine which parts can be styled differently.
+   * options: {
+   *     multiOptions: {
+   *         optionIdxFn: function (latLng, prevLatLng, index, allLatlngs),
+   *         fnContext: ctx, // the context to call optionIdxFn (optional)
+   *         options: [{}, {}, {}] or function, // options for the index returned by optionIdxFn. If supplied with a function then it will be called with the index
+   *         copyBaseOptions: true
+   *     },
+   *     // other options from Polyline
+   * }
+   */
+  L.MultiOptionsPolyline = L.FeatureGroup.extend({
+    initialize: function(latlngs, options) {
+      this._layers = {};
+      this._latlngs = latlngs;
 
-    initialize: function (latlngs, options) {
-        this._layers = {};
-        this._latlngs = latlngs;
+      this._prepareOptions(options);
 
-        this._options = options;
-
-        var copyBaseOptions = options.multiOptions.copyBaseOptions;
-        if (copyBaseOptions === undefined || copyBaseOptions) {
-            this._copyBaseOptions();
-        }
-
-        this.render();
+      this.render();
     },
 
-    _copyBaseOptions: function () {
+    _prepareOptions: function(options) {
+      this._options = options;
+      var copyBaseOptions = options.multiOptions.copyBaseOptions;
+      if (copyBaseOptions === undefined || copyBaseOptions) {
         var multiOptions = this._options.multiOptions,
-            baseOptions,
-            optionsArray = multiOptions.options,
-            i, len = optionsArray.length;
+          baseOptions,
+          optionsArray = multiOptions.options,
+          i, len = optionsArray.length;
 
         baseOptions = L.extend({}, this._options);
         delete baseOptions.multiOptions;
 
         for (i = 0; i < len; ++i) {
-            optionsArray[i] = L.extend({}, baseOptions, optionsArray[i]);
+          optionsArray[i] = L.extend({}, baseOptions, optionsArray[i]);
         }
+      }
     },
 
-    setLatLngs: function (latlngs) {
-        this._latlngs = latlngs;
-        this.render();
+    setLatLngs: function(latlngs) {
+      this._latlngs = latlngs;
+      this.render();
     },
 
-    setOptions: function (options) {
-        var copyBaseOptions = options.multiOptions.copyBaseOptions;
-        this._options = options;
-        if (copyBaseOptions === undefined || copyBaseOptions) {
-            this._copyBaseOptions();
-        }
-        this.render();
+    setOptions: function(options) {
+      this._prepareOptions(options);
+
+      this.render();
     },
 
-    render: function () {
-        var i, len = this._latlngs.length,
-            multiOptions = this._options.multiOptions,
-            optionIdxFn = multiOptions.optionIdxFn,
-            fnContext = multiOptions.fnContext || this,
-            prevOptionIdx, optionIdx,
-            segmentLatlngs;
+    render: function() {
+      var i, len = this._latlngs.length,
+        multiOptions = this._options.multiOptions,
+        optionIdxFn = multiOptions.optionIdxFn,
+        fnContext = multiOptions.fnContext || this,
+        prevOptionIdx, optionIdx,
+        segmentLatlngs;
 
-        this.eachLayer(function (layer) {
-            this.removeLayer(layer);
-        }, this);
+      this.eachLayer(function(layer) {
+        this.removeLayer(layer);
+      }, this);
 
-        for (i = 1; i < len; ++i) {
-            optionIdx = optionIdxFn.call(
-                fnContext, this._latlngs[i], this._latlngs[i - 1], i, this._latlngs);
+      for (i = 1; i < len; ++i) {
+        optionIdx = optionIdxFn.call(
+          fnContext, this._latlngs[i], this._latlngs[i - 1], i, this._latlngs);
 
-            if (i === 1) {
-                segmentLatlngs = [this._latlngs[0]];
-                prevOptionIdx = optionIdx;
-            }
-
-            segmentLatlngs.push(this._latlngs[i]);
-
-            // is there a change in options or is it the last point?
-            if (prevOptionIdx !== optionIdx || i === len - 1) {
-                //Check if options is a function or an array
-                if(typeof multiOptions.options == "function"){
-                    this.addLayer(new L.Polyline(segmentLatlngs, multiOptions.options(prevOptionIdx)));
-                }else{
-                    this.addLayer(new L.Polyline(segmentLatlngs, multiOptions.options[prevOptionIdx]));
-                }
-
-                prevOptionIdx = optionIdx;
-                segmentLatlngs = [this._latlngs[i]];
-            }
+        if (i === 1) {
+          segmentLatlngs = [this._latlngs[0]];
+          prevOptionIdx = optionIdx;
         }
 
-        return this;
+        segmentLatlngs.push(this._latlngs[i]);
+
+        // is there a change in options or is it the last point?
+        if (prevOptionIdx !== optionIdx || i === len - 1) {
+          //Check if options is a function or an array
+          if (typeof multiOptions.options == "function") {
+            this.addLayer(new L.Polyline(segmentLatlngs, multiOptions.options(prevOptionIdx)));
+          } else {
+            this.addLayer(new L.Polyline(segmentLatlngs, multiOptions.options[prevOptionIdx]));
+          }
+
+          prevOptionIdx = optionIdx;
+          segmentLatlngs = [this._latlngs[i]];
+        }
+      }
+
+      return this;
     },
 
-    getLatLngs: function () {
-        return this._latlngs;
+    getLatLngs: function() {
+      return this._latlngs;
     },
 
-    getLatLngsSegments: function () {
-        var latlngs = [];
+    getLatLngsSegments: function() {
+      var latlngs = [];
 
-        this.eachLayer(function (layer) {
-            latlngs.push(layer.getLatLngs());
-        });
+      this.eachLayer(function(layer) {
+        latlngs.push(layer.getLatLngs());
+      });
 
-        return latlngs;
+      return latlngs;
     }
-});
+  });
 
-L.multiOptionsPolyline = function (latlngs, options) {
+  L.multiOptionsPolyline = function(latlngs, options) {
     return new L.MultiOptionsPolyline(latlngs, options);
-};
+  };
 
 }());
